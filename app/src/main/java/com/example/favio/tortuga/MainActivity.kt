0c9e9f7b5aeb92ec636d044ebb59baa071831c8c
favio999 <favio999.fq@gmail.com>
2019-05-25 05:18:18 +0100

    finalizado

---
+++ app/src/main/java/com/example/favio/tortuga/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.favio.tortuga
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun radiobuttonClick(view: View){
+
+        if(view == rb_leo){
+
+            tortuga.setImageResource(R.drawable.tmntleo)
+        }
+        if(view == rb_don){
+
+            tortuga.setImageResource(R.drawable.tmntdon)
+        }
+        if(view == rb_mike){
+
+            tortuga.setImageResource(R.drawable.tmntmike)
+        }
+        if(view == rb_traph){
+
+            tortuga.setImageResource(R.drawable.tmntraph)
+        }
+
+
+    }
+}
